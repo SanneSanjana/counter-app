df94de083bc9fd94eedb2f94f9f6ee4981c303c1
SanneSanjana <134002721+SanneSanjana@users.noreply.github.com>
2026-01-21 18:17:42 +0530

    Added  counter component and styles

---
+++ src/components/Counter/counter.js
@@ -0,0 +1,54 @@
+// import "./index.css"
+// import React, { useState } from "react";
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   const increase = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrease = () => {
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Counter</h2>
+//       <h3>{count}</h3>
+
+//       <button onClick={increase}>Increase</button>
+//       <button onClick={decrease}>Decrease</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+import "./index.css";
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="container">
+      <h2 className="heading">Counter</h2>
+      <h3 className="count">{count}</h3>
+
+      <div>
+        <button className="button" onClick={increase}>Increase</button>
+        <button className="button" onClick={decrease}>Decrease</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
